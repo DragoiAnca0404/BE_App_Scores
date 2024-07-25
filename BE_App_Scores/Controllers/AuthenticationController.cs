766a7275a39c31b1bde3f16a88b76c033c4358d0
Dragoi Anca-Florentina <67691293+DragoiAnca0404@users.noreply.github.com>
2024-07-25 15:41:59 +0300

    Creating Login API

--- BE_App_Scores/Controllers/AuthenticationController.cs
+++ BE_App_Scores/Controllers/AuthenticationController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using BE_App_Scores.Models;
 using BE_App_Scores.Service.Models;
 using BE_App_Scores.Service.Services;
+using BE_App_Scores.Models.Authentication.Login;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace BE_App_Scores.Controllers
 {
     [Route("api/[controller]")]
@@ -18,14 +24,18 @@ namespace BE_App_Scores.Controllers
 
         public AuthenticationController(UserManager<IdentityUser> userManager,
             RoleManager<IdentityRole> roleManager,
-            IEmailService emailService)
+            IEmailService emailService,
+            IConfiguration configuration
+            )
         {
             _userManager = userManager;
             _roleManager = roleManager;
             _emailService = emailService;
+            _configuration = configuration;
         }
 
         [HttpPost]
+        [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
         {
 
@@ -112,6 +122,46 @@ namespace BE_App_Scores.Controllers
 
             return StatusCode(StatusCodes.Status500InternalServerError, 
                 new Response { Status = "Error", Message = "This user doesn't exist!" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                var token = GetToken(authClaims);
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
         }
     }
 }
---
+++ BE_App_Scores/Models/Authentication/Login/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BE_App_Scores.Models.Authentication.Login
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="UserName is required!")]
+        public string? Username {  get; set; }
+        [Required(ErrorMessage ="Password is required")]
+        public string? Password {  get; set; }
+    }
+}
