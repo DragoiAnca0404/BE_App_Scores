f3ed56547e3671d169ce4c412473a4f47510d0fd
Dragoi Anca-Florentina <67691293+DragoiAnca0404@users.noreply.github.com>
2024-09-03 15:24:45 +0300

    implement who win the game

--- BE_App_Scores/Models/Authentication/Login/LoginModel.cs
+++ BE_App_Scores/Models/Authentication/Login/LoginModel.cs
@@ -5,7 +5,7 @@ namespace BE_App_Scores.Models.Authentication.Login
     public class LoginModel
     {
         [Required(ErrorMessage ="UserName is required!")]
-        public string? Username {  get; set; }
+        public string? UsernameOrEmail { get; set; }
         [Required(ErrorMessage ="Password is required")]
         public string? Password {  get; set; }
     }
